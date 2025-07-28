e4afb8d51eea054ad9f188bf79c2b661cf74231d
withefbmobile2012 <with.efb.mobile2012@gmail.com>
2025-07-28 09:36:03 +0300

    users

--- main/views/users.py
+++ main/views/users.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 import users.forms as forms
 
 
 def register_user(request):
+    if request.POST:
+        form = forms.RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('/')
     form = forms.RegisterUserForm()
-    return render(request, "form.html", {"form": form})
+    return render(request, "auth/register.html", {"form": form})
