ef519aaaf0562074bd97c2a54aa847e4a7fc50f1
Muhammad Aziz <richuzb13@gmail.com>
2025-07-28 10:28:44 +0500

    expense.py fixed

--- main/views/expense.py
+++ main/views/expense.py
@@ -2,8 +2,10 @@ from django.views import View
 from django.shortcuts import render
 from main.views import *
 from main.models.expense import Expense
+from main.models import base
 
-class Expense(View):
+
+class Expense(base.BaseModel):
     def get(self, request):
         expenses = Expense.objects.all()
         return render(request, 'expense_list.html', {'expenses': expenses})
