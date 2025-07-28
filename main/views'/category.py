acc8a10f327e11c8198f06b4bcff296768429d4a
Muhammaddiyor443 <hamidovmuhammaddier443@gmail.com>
2025-07-28 09:58:42 +0500

    Add category

--- main/views'/category.py
+++ main/views'/category.py
@@ -1,10 +1,28 @@
-from decorator import decorator
-from django.views.generic import TemplateView
 from django.shortcuts import redirect, render, get_object_or_404
 
 import main.models as models
 
-def category_list(request):
-    categories = models.Category.objects.all()
-    return render(request, "categories.html")
+categories = [
 
+]
+
+
+def category_c(request):
+    category_cr = models.Category.objects.all()
+
+    if request.POST:
+        name = request.POST.get("name")
+        models.User.objects.create(
+            name=name,
+        )
+        categories.append(category_cr)
+
+    return render(request, "categories.html", context={"created": category_cr})
+
+def category_r(request):
+    category_read = categories
+
+    if request.POST:
+        return category_read
+
+    return render(request, "categories.html", context={"created": category_cr})
