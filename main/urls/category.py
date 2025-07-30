167e1b01b3c4ebf1d05e48f3d6c8de20beae4de8
Muhammaddiyor443 <hamidovmuhammaddier443@gmail.com>
2025-07-30 10:15:52 +0500

    fix

--- main/urls/category.py
+++ main/urls/category.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from main import views
-from main.views import category
+from main.views import category as views
 
 
 urlpatterns = [
-    path('', views.category_list, name='category_list'),
-    path('<int:pk>/', views.category_detail, name='category_detail'),
+    path('', views.category_read, name='category_list'),
     path('create/', views.category_create, name='category_create'),
     path('<int:pk>/update/', views.category_update, name='category_update'),
     path('<int:pk>/delete/', views.category_delete, name='category_delete'),
