b5e8749afef1fe0186f5654657c7fc4fcc7c111f
Muhammaddiyor443 <hamidovmuhammaddier443@gmail.com>
2025-07-30 11:12:30 +0500

    fix

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
-    list_filter = ('is_staff', 'is_active')
-    ordering = ('username',)
-    
+import main.models as models
 
+
+# Register your models here.
+class BaseAdmin(admin.ModelAdmin):
+    search_fields = ("name", "username", "description")
+    list_filter = ("added_at",)
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "added_at")
+    list_display_links = ("id", "name")
+
+
+@admin.register(models.Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("id", "amount", "added_at", "description", "date")
+    list_display_links = ("id", "description")
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    ...
