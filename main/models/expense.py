bd101d3ac429893184e9725542f192400f2a3881
Muhammad Aziz <richuzb13@gmail.com>
2025-08-01 09:40:48 +0500

    category fix

--- main/models/expense.py
+++ main/models/expense.py
@@ -8,7 +8,7 @@ class Expense(base.BaseModel):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255, blank=True, null=True)
     date = models.DateField(default=timezone.now)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.description or 'Expense'} - {self.amount} on {self.date}"
