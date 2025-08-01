33799456250ee37ba54094d57fd14f07c16010b7
Muhammad Aziz <richuzb13@gmail.com>
2025-08-01 09:34:14 +0500

    bunch of stuff

--- main/models/expense.py
+++ main/models/expense.py
@@ -1,13 +1,14 @@
 from django.db import models
 from main.models import base
 from django.utils import timezone
-
+from main.models.category import Category
 
 
 class Expense(base.BaseModel):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.CharField(max_length=255, blank=True, null=True)
     date = models.DateField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.description or 'Expense'} - {self.amount} on {self.date}"
