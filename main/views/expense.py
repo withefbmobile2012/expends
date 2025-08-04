643538376af4d1f53a81d6bea9ad892a6bdb0824
Muhammad Aziz <richuzb13@gmail.com>
2025-08-04 11:12:32 +0500

    fuck

--- main/views/expense.py
+++ main/views/expense.py
@@ -6,6 +6,7 @@ from main.forms import ExpenseForm
 
 
 
+
 class ExpenseView(View):
     def get(self, request):
         form = ExpenseForm()
@@ -20,3 +21,21 @@ class ExpenseView(View):
         
         expenses = Expense.objects.all()
         return render(request, 'expense.html', {'form': form, 'expenses': expenses})
+    
+
+class ExpenseUpdateView(View):
+    def get(self, request, pk):
+        expense = Expense.objects.get(pk=pk)
+        form = ExpenseForm(instance=expense)
+        return render(request, 'expense_update.html', {'form': form, 'expense': expense})
+    
+    
+class ExpenseDeleteView(View):
+    def get(self, request, pk):
+        expense = Expense.objects.get(pk=pk)
+        return render(request, 'expense_delete.html', {'expense': expense})
+    
+    def post(self, request, pk):
+        expense = Expense.objects.get(pk=pk)
+        expense.delete()
+        return redirect('/expense')
