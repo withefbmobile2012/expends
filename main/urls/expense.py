2b29af514683ac7038e737641cf2ea8fd8d2af4a
Muhammad Aziz <richuzb13@gmail.com>
2025-08-06 12:15:47 +0500

    working delete/update

--- main/urls/expense.py
+++ main/urls/expense.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from main.views.expense import ExpenseView, ExpenseUpdateView, ExpenseDeleteView
+from main.views.expense import ExpenseView, ExpenseEditView, ExpenseDeleteView
 
 urlpatterns = [
     path('expense/', ExpenseView.as_view(), name='expense_list'),
     path('expense/<int:id>/', ExpenseView.as_view(), name='expense_detail'),
-    path('expense/<int:id>/edit/', ExpenseView.as_view(), name='expense_edit'),
-    path('expense/update/<int:pk>/', ExpenseUpdateView.as_view(), name='expense_update'),
-    path('expense/delete/<int:pk>/', ExpenseDeleteView.as_view(), name='expense_delete'),
+    path('expense/<int:pk>/edit/', ExpenseEditView.as_view(), name='expense_edit'),
+    path('expense/<int:pk>/delete/', ExpenseDeleteView.as_view(), name='expense_delete'),
 ]
--- main/views/expense.py
+++ main/views/expense.py
@@ -3,41 +3,47 @@ from main.models.expense import Expense
 from main.views import *
 from django.views import View
 from main.forms import ExpenseForm
-from django.views.generic import UpdateView
-from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 
 
 
 class ExpenseView(View):
     def get(self, request):
-        form = ExpenseForm()
         expenses = Expense.objects.all()
-        return render(request, 'index.html', {'form': form, 'expenses': expenses})
+        return render(request, 'index.html', {
+            'form': ExpenseForm(),
+            'expenses': expenses
+        })
 
     def post(self, request):
         form = ExpenseForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/expense') 
-        
-        expenses = Expense.objects.all()
-        return render(request, 'expense.html', {'form': form, 'expenses': expenses})
-    
+        return redirect('expense_list')
 
-class ExpenseUpdateView(UpdateView):
-    model = Expense
-    form_class = ExpenseForm
-    template_name = 'expense_update.html'
-    success_url = reverse_lazy('expense_list')
-    
+
+class ExpenseEditView(View):
+    def get(self, request, pk):
+        expenses = Expense.objects.all()
+        expense = get_object_or_404(Expense, pk=pk)
+        return render(request, 'index.html', {
+            'form': ExpenseForm() if pk is None else None,  # no add form while editing
+            'expenses': expenses,
+            'edit_form': ExpenseForm(instance=expense),
+            'edit_id': pk
+        })
+
+    def post(self, request, pk):
+        expense = get_object_or_404(Expense, pk=pk)
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+        return redirect('/expense')
+
 
 class ExpenseDeleteView(View):
-    def get(self, request, pk):
-        expense = Expense.objects.get(pk=pk)
-        return render(request, 'expense_delete.html', {'expense': expense})
-    
     def post(self, request, pk):
-        expense = Expense.objects.get(pk=pk)
+        expense = get_object_or_404(Expense, pk=pk)
         expense.delete()
         return redirect('/expense')
