022d32c1ddee0f883b56a89ee86ef83bcb3d26e0
withefbmobile2012 <with.efb.mobile2012@gmail.com>
2025-08-08 10:31:29 +0300

    mm

--- main/urls/users.py
+++ main/urls/users.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from main.views import users
-
+import main.views.users as views
 urlpatterns = [
-
+    path('register', views.register_user, name='register'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('profile', views.profile, name='profile'),
 ]
