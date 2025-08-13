ba73c2823da811d2de24f968787493c778bb7911
Muhammaddiyor443 <hamidovmuhammaddier443@gmail.com>
2025-08-13 08:52:08 +0500

    category

--- main/urls/category.py
+++ main/urls/category.py
@@ -3,9 +3,9 @@ from main.views import category as views
 
 
 urlpatterns = [
-    path('', views.categories_list, name='category_list'),
-    path('create/', views.category_create, name='category_create'),
-    path('<int:pk>/update/', views.category_update, name='category_update'),
-    path('<int:pk>/delete/', views.category_delete, name='category_delete'),
-    path('int:pk/detail/', views.categories_detail, name='category_detail'),
+    path('', views.categories_list, name='category'),
+    path('create/', views.category_create, name='category'),
+    path('<int:pk>/update/', views.category_update, name='category'),
+    path('<int:pk>/delete/', views.category_delete, name='category'),
+    path('int:pk/detail/', views.categories_detail, name='category'),
 ]
